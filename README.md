e08f0ab36bef58d4019d863e115929c5b75734db
cpituello <cedric.pituello@isen.yncrea.fr>
2017-11-20 10:08:09 +0100

    no message

---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# Projet_Worms
+
+![Worms](/SHES/readme_screen.jpg)
+
+## Contexte du projet
+
+### Qu'est ce que le jeu Worms ? ###
+
+« Joué au tour par tour, en 2 dimensions, il oppose deux équipes de vers de terre armés jusqu'aux dents, selon le principe du jeu d'artillerie. Le concept de jeu a été inventé par Andy Davison.
+
+Le but étant de détruire tous ses ennemis, on y trouve des armes destructrices à souhait, plus ou moins conventionnelles, allant du bazooka, jusqu'au fusil en passant par le coup de poing de feu ou le mouton explosif.
+Ces armes enlèvent de la vie au(x) ver(s) touché(s) ; réduire les points de vie des vers ennemis étant le principal moyen de gagner.
+Cependant, on peut aussi envoyer les lombrics dans l'eau où ils se noient aussitôt.
+
+Certaines armes sont d'ailleurs prévues pour ça, comme la très utilisée batte de baseball. Sortir un ver des limites du jeu permet également de l'éliminer. Un inventaire d'outils permet également de creuser le décor, de s'agripper au mur ou encore de poser des mines. »
+
+*Source : Wikipédia*
+
+## Objectif du projet
+
+L'objectif du projet est donc de développer un **clone de Worms**. Les aspects importants à gérer seront la physique des vers avec le décor, ses déplacements, la gestion et l'animation des tirs, la map destructible et l'interface, entre autres. Le jeu sera codé en utilisant le C# en utilisant le framework **Monogame**.
---
+++ Technique/ProjetWorms/SimpleAnimationSprite.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjetWorms
+{
+    class SimpleAnimationSprite
+    {
+        private Point position; //position d'affichage
+        private Vector2 origin;
+        protected Game Game;
+        public SimpleAnimationDefinition Definition;
+        private SpriteEffects spriteEffects;
+        private SpriteBatch spriteBatch;
+        protected Texture2D sprite;
+        protected Point CurrentFrame;
+        private bool finishedAnimation = false;
+        protected double TimeBetweenFrame = 16; // 60 fps 
+        protected double lastFrameUpdatedTime = 0;
+        private int _Framerate = 60;
+        public int Framerate
+        {
+            get { return this._Framerate; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Framerate can't be less or equal to 0");
+                if (this._Framerate != value)
+                {
+                    this._Framerate = value;
+                    this.TimeBetweenFrame = 1000.0d / (double)this._Framerate;
+                }
+            }
+        }
+        private float rotationAngle = 0;
+
+        public Point Position { get => position; set => position = value; }
+
+        public bool FinishedAnimation { get => finishedAnimation; set => finishedAnimation = value; }
+        public float RotationAngle { get => rotationAngle; set => rotationAngle = value; }
+        public SpriteEffects SpriteEffects { get => spriteEffects; set => spriteEffects = value; }
+
+        /* Proprietes */
+        public SimpleAnimationSprite(Game game, SimpleAnimationDefinition definition)
+        {
+            /* Constructeur */
+            this.Game = game;
+            this.Definition = definition;
+            this.Position = new Point();
+            this.CurrentFrame = new Point();
+        }
+
+        public void Initialize()
+        {
+            /* Initialisation */
+            this.Framerate = this.Definition.FrameRate;
+            this.CurrentFrame.X = 0;
+            this.CurrentFrame.Y = 0;
+        }
+
+        public void LoadContent(SpriteBatch spritebatch)
+        {
+            /* Chargements des donnees */
+            this.sprite = this.Game.Content.Load < Texture2D>(this.Definition.AssetName);
+            this.spriteBatch = spritebatch;
+            this.origin.X = 0;
+            this.origin.Y = 0;
+            this.spriteEffects = SpriteEffects.None;
+        }
+
+        public void Reset()
+        {
+            /* Reinitialisation de l'animation */
+            this.CurrentFrame = new Point();
+            this.FinishedAnimation = false;
+            this.lastFrameUpdatedTime = 0;
+        }
+
+        public void Update(GameTime time)
+        {
+            /* Mise a jour des donnees en vue de l'affichage */
+            if (FinishedAnimation) return;
+            this.lastFrameUpdatedTime += time.ElapsedGameTime.Milliseconds;
+            if (this.lastFrameUpdatedTime > this.TimeBetweenFrame)
+            {
+                this.lastFrameUpdatedTime = 0;
+                if (this.Definition.Loop)
+                {
+                    this.CurrentFrame.X++;
+                    if (this.CurrentFrame.X >= this.Definition.NbFrames.X)
+                    {
+                        this.CurrentFrame.X = 0;
+                        this.CurrentFrame.Y++;
+                        if (this.CurrentFrame.Y >= this.Definition.NbFrames.Y)
+                            this.CurrentFrame.Y = 0;
+                    }
+                }
+                else
+                {
+                    this.CurrentFrame.X++;
+                    if (this.CurrentFrame.X >= this.Definition.NbFrames.X)
+                    {
+                        this.CurrentFrame.X = 0;
+                        this.CurrentFrame.Y++;
+                        if (this.CurrentFrame.Y >= this.Definition.NbFrames.Y)
+                        {
+                            this.CurrentFrame.X = this.Definition.NbFrames.X - 1;
+                            this.CurrentFrame.Y = this.Definition.NbFrames.Y - 1;
+                            this.FinishedAnimation = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Draw(GameTime time)
+        {
+            /* Affichage de l'animation */
+            this.spriteBatch.Draw(this.sprite,
+                                    new Rectangle(this.Position.X, this.Position.Y, this.Definition.FrameSize.X, this.Definition.FrameSize.Y),
+                                    new Rectangle(this.CurrentFrame.X * this.Definition.FrameSize.X, this.CurrentFrame.Y * this.Definition.FrameSize.Y, this.Definition.FrameSize.X, this.Definition.FrameSize.Y),
+                                    Color.White, rotationAngle, origin, spriteEffects,0f);
+        }
+    }
+}
